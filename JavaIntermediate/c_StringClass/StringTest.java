e4c52a583d01c9edf453a9f53f6f47f27085d716
daniel <hun320daniel07@gmail.com>
2026-01-20 23:18:37 +0900

    Java intermediate String Class 공부

---
+++ JavaIntermediate/c_StringClass/StringTest.java
@@ -0,0 +1,10 @@
+package c_StringClass;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        System.out.println(str1);
+        str1.concat("World");
+        System.out.println(str1);
+    }
+}
