12b1bfbad70bdd00ccae28d93e92dc1ff0d843fa
daniel <hun320daniel07@gmail.com>
2026-01-28 16:48:45 +0900

    Java Intermediate 2 ArrayList

---
+++ JavaIntermediate2/arrayList/Main.java
@@ -0,0 +1,15 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(10);
+        list1.add(20);
+        list1.add(30);
+        System.out.println(list1.size());
+    }
+
+}
